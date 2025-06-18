ef2f08716050c1da36e67eeeb01db2a4c51a9fe4
Anjana T A <99anjanaanil@gmail.com>
2025-06-18 23:33:25 +0100

    cleaned code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ model_checkpoint = "ppo_checkpoints"
 # run_ppo_training("Salesforce/codegen-2B-multi", datasets["train"],100)
 # run_ppo_training("bigcode/starcoder2-3b", datasets["train"],100)
 # run_ppo_batch_training("bigcode/starcoder2-3b", datasets["train"],100)
-run_ppo_epoch_training("Salesforce/codet5-base",output_dir="./ppo_checkpoints", 
+run_ppo_epoch_training("bigcode/starcoder2-3b",output_dir="./ppo_checkpoints", 
     batch_size=2, 
     num_epochs=3,  # Added epoch control
     eval_every=1  )
